6fd74f30b2a82e98c4c89947f7a303e334be517b
Abdullah-Al-Turky <47826280+coderabdullahturki@users.noreply.github.com>
2022-04-05 11:24:27 +0600

    updated css

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React, { useState } from 'react';
+import Clock from './Components/Clock';
+
+
+function App() {
+  const [timerDays, setTimerDays] = useState();
+  const [timerHours, setTimerHours] = useState();
+  const [timerMinutes, setTimerMinutes] = useState();
+  const [timerSeconds, setTimerSeconds] = useState();
+  return (
+    <div className="App">
+      <Clock 
+        timerDays={timerDays}
+        timerHours={timerHours}
+        timerMinutes={timerMinutes}
+        timerSeconds={timerSeconds}
+      />
+    </div>
+  );
+}
+
+export default App;
